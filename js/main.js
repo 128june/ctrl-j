bf857b6a8f5e8a81a3d54285fb839b821dddbb5d
128june <128june@hanmail.net>
2025-10-26 16:40:20 +0900

    web init

---
+++ js/main.js
@@ -0,0 +1,4 @@
+// 메인 자바스크립트
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('ctrl-j 웹페이지가 로드되었습니다.');
+});
---
+++ js/utils.js
@@ -0,0 +1,7 @@
+// 유틸리티 함수들
+const utils = {
+    // 예시 함수
+    formatDate: function(date) {
+        return new Date(date).toLocaleDateString('ko-KR');
+    }
+};
